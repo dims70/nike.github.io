c748072d9eab5f4e977030eb21eefd829f6c5a8e
dims70 <shkarpetovd@mail.ru>
2020-08-28 15:56:46 +0300

    Небольшие изменения

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,13 @@ cards.forEach((card,i)=>{
     card.onmouseenter = ()=>{
             card.children[0].style.display="none";
             card.children[1].innerHTML=
-            "<div class='item-subtitle active'>"+
+            "<div class='item-subtitle activep'>"+
             "<span>Nike Airmax 2090</span>"+
             "<p>Shown:White/Pure Platinum<br>Bright Crimson/Black</p>"+
-            "<p>Style: СТ7698-100</p>"
+            "<p>Style: СТ7698-100</p>" +
+            "<button>Больше</button>" +
             "</div>";
-            document.querySelectorAll('.pagins li')[i].classList="active";
+            document.querySelectorAll('.pagins li')[i].classList="activep";
         };
     card.onmouseleave = ()=>{
             card.children[0].style.display="block";
@@ -19,3 +20,8 @@ cards.forEach((card,i)=>{
             document.querySelectorAll('.pagins li')[i].classList="";
         };
 });
+
+let searchicon = document.querySelector('.header-search img');
+searchicon.onmouseenter=()=>{
+    
+}
